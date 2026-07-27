4b15a6c52bfb7de22a3c48fd799fdffbba924fdf
Anil Mandyal <46489249+code-with-anil-mandyal@users.noreply.github.com>
2026-07-27 11:27:23 +0530

    Update API token to use placeholder
    Replaced hardcoded token with a placeholder for security.

--- shared/src/commonMain/kotlin/com/codewithmandyal/movie_explorer/data/remote/NetworkConstants.kt
+++ shared/src/commonMain/kotlin/com/codewithmandyal/movie_explorer/data/remote/NetworkConstants.kt
@@ -5,7 +5,7 @@ object NetworkConstants {
     const val PROFILE_PIC_BASE_URL = "https://image.tmdb.org/t/p/w185"
     const val BANNER_PIC_BASE_URL = "https://image.tmdb.org/t/p/w780"
     const val MOVIE_CARD_PIC_BASE_URL = "https://image.tmdb.org/t/p/w500"
-    const val TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMWY1ZGJhMzlmZTVkNWU3ZDY1NzViOGE1NjI4ZmZmNSIsIm5iZiI6MTc4NDE5NTIyNi4xNjk5OTk4LCJzdWIiOiI2YTU4YTg5YWFiNzg1ODhkY2Y0YTU5YTciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.2pDPj7e1_SGRypUaboXM7lOcG7oS0_-f2EA2To_bIuk"
+    const val TOKEN = "YOUR_AUTH_TOKEN"
     const val DISCOVER = "discover"
     const val TOP_RATED = "top_rated"
 
